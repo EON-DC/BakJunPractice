ebdebc7c0169300c6e9845ec187e1f5f8cf8fedb
EON-DC <mun0392@gmail.com>
2022-10-13 16:35:35 +0900

    알고리즘 공부 commit 프로그래머스 병행`

--- src/Programmers/P_12937.java
+++ src/Programmers/P_12937.java
@@ -5,17 +5,16 @@ import java.util.Arrays;
 
 public class P_12937 {
 
-    class Solution {
-        public String solution(int num) {
-            String answer = "";
-            if(num < 0){
-                num *= -1;
-            }
-            if(num % 2==1){
-                answer = "Odd";
-            }else{
-                answer = "Even";
-            }
-            return answer;
+    public String solution(int num) {
+        String answer = "";
+        if (num < 0) {
+            num *= -1;
         }
-    }}
+        if (num % 2 == 1) {
+            answer = "Odd";
+        } else {
+            answer = "Even";
+        }
+        return answer;
+    }
+}
